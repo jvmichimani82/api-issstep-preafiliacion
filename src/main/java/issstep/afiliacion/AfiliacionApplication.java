2fa9609b5c24515a3af0066a1ab1ef8653950288
Jose Valencia Michimani <jv.michimani82@gmail.com>
2019-05-27 15:39:43 -0500

    Initial commit

---
+++ src/main/java/issstep/afiliacion/AfiliacionApplication.java
@@ -0,0 +1,13 @@
+package issstep.afiliacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfiliacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfiliacionApplication.class, args);
+	}
+
+}
